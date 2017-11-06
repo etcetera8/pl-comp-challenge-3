bd78fe2cbd7608839eaa78459a65f058b90a4669
parker lindley <fugazi8@gmail.com>
2017-11-05 19:57:19 -0700

    Add more styling to table

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 $('#hamburger-div').on('click', function() {
   $('.navbar').slideToggle('slow');
 })
+
+$('.fa-chevron-circle-up').on('click', function() {
+  $('#graph-display, .buttons').slideToggle('slow');
+})
